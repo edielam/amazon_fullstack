40add98bdb71b183aab507e2a877a638d12dacca
edielam <edguard738@gmail.com>
2021-10-09 19:55:44 +0000

    react-frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,121 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container"> 
+            <header className="header">
+                <div className="brand">
+                    <button onclick="openMenu()">
+                        &#9776;
+                    </button>
+                    <a href="index.html">Camazon</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Login</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onclick="closeMenu()">x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Pants</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Pants</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <ul className="products">
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d3.jpg" alt="product"/>
+                                <div className="product-name">
+                                    <a href="products.html">Slim Shirt</a></div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">GHS 500</div>
+                                <div className="product-rating">4.5 Stars(10 reviews)</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+            <footer className="footer">
+                All rights reserved
+            </footer>
+        </div>
   );
 }
 
