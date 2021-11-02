6100207f956b4e4119f11d38f751780cc0acbbde
edielam <edguard738@gmail.com>
2021-11-02 19:35:36 +0000

    created api server

---
+++ backend/data.js
@@ -0,0 +1,85 @@
+const Data = {
+    products:[{
+        _id: 1,
+        name: 'Slim Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    },
+    {
+        _id: 2,
+        name: 'Fit Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    },
+    {
+        _id: 3,
+        name: 'mango Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: '4.0',
+        numReviews: 10
+    },
+    {
+        _id: 4,
+        name: 'Brown Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 800,
+        brand: 'Nike',
+        rating: '3.5',
+        numReviews: 10
+    },
+    {
+        _id: 5,
+        name: 'muy Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    },
+    {
+        _id: 6,
+        name: 'woman Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    },
+    {
+        _id:7,
+        name: 'muyxs Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    },
+    {
+        _id: 8,
+        name: 'man Shirt',
+        category: 'Shirt',
+        image: '/images/d3.jpg',
+        price: 500,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: 10
+    }
+]
+}
+
+export default Data;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import Data from './data';
+const app = express();
+
+app.get("/api/products", (req, res) => {
+    res.send(Data.products);
+});
+
+app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
