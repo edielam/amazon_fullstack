4200f63e50fa184e003cb721b8fcd726144d8fcc
edielam <edguard738@gmail.com>
2021-11-03 10:40:33 +0000

    managing state with redux

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,11 +1,11 @@
 import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
-function productListReducer(state = { products: []}, action) {
+function productListReducer(state = { mYproducts: []}, action) {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return {loading: true};
         case PRODUCT_LIST_SUCCESS:
-            return {loading: false, products: action.payload};
+            return {loading: false, mYproducts: action.payload};
         case PRODUCT_LIST_FAIL:
             return { loading: false, error: action.payload }
         default:
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,42 +1,44 @@
 import {Link} from "react-router-dom"
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
 
     /*creating hooks for fetching data from api*/
-    const [myProduct, setProduct] = useState([]);
+    const productList = useSelector(state => state.productList);
+    const {mYproducts, loading, error} = productList;
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("api/products");
-            setProduct(data);
-        }
-        fetchData();
+        dispatch(listProducts());
         return () => {
             //
         };
     }, [])
 
 
-    return <ul className="products">
-    {
-        myProduct.map(product => 
-            <li key={product._id}>
-                <div className="product">
-                    <Link to={'/product/' + product._id}>
-                        <img className="product-image" src={product.image} alt="product"/>
-                    </Link>
-                    <div className="product-name">
-                        <Link to={'/product/' + product._id}>{product.name}</Link>
+    return loading? <div>Loading...</div> :
+    error? <div>{error}</div> :
+
+        <ul className="products">
+        {
+            mYproducts.map(product => 
+                <li key={product._id}>
+                    <div className="product">
+                        <Link to={'/product/' + product._id}>
+                            <img className="product-image" src={product.image} alt="product"/>
+                        </Link>
+                        <div className="product-name">
+                            <Link to={'/product/' + product._id}>{product.name}</Link>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">GHS {product.price}</div>
+                        <div className="product-rating">{product.rating} Stars {product.numReviews}</div>
                     </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">GHS {product.price}</div>
-                    <div className="product-rating">{product.rating} Stars {product.numReviews}</div>
-                </div>
-            </li>)
-    }
-    </ul>
+                </li>)
+        }
+        </ul>
 }
 export default HomeScreen;
 
