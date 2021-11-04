d96a24c98ebc97431c696b44f80d3d1ca1d6a75d
edielam <edguard738@gmail.com>
2021-11-04 08:27:19 +0000

    adding redux to product details

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,15 +1,25 @@
 import axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { PRODUCT_DET_FAIL, PRODUCT_DET_REQUEST, PRODUCT_DET_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
 
 const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST});
         const {data} = await axios.get("/api/products");
-        dispatch({type: PRODUCT_LIST_SUCCESS, payload: data})
+        dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
         
     } catch (error) {
         dispatch({type: PRODUCT_LIST_FAIL, payload:error.message})
     }   
 }
 
-export { listProducts }
+const detProduct = (productId) => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DET_REQUEST, payload: productId});
+        const {deta} = await axios.get("/api/products/" + productId);
+        dispatch({type: PRODUCT_DET_SUCCESS, payload: deta});
+    } catch (error) {
+        dispatch({type: PRODUCT_DET_FAIL, payload:error.message})
+    }
+}
+
+export { listProducts, detProduct }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_DET_FAIL, PRODUCT_DET_REQUEST, PRODUCT_DET_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 function productListReducer(state = { mYproducts: []}, action) {
     switch (action.type) {
@@ -13,4 +13,18 @@ function productListReducer(state = { mYproducts: []}, action) {
     }
 }
 
-export {productListReducer}
+function productDetReducer(state = { product: {}}, action) {
+    switch (action.type) {
+        case PRODUCT_DET_REQUEST:
+            return {loading: true};
+        case PRODUCT_DET_SUCCESS:
+            return {loading: false, product: action.payload};
+        case PRODUCT_DET_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state;
+    }
+}
+
+
+export {productListReducer, productDetReducer}
